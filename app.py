420269010cae5870675c9b490878a0ad6471eb41
Pramodnani <48893435+Pramodnani@users.noreply.github.com>
2020-03-17 02:39:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-#     int_features = [int(x) for x in request.form.values()]
-#     final_features = [np.array(int_features)]
+     int_features = [int(x) for x in request.form.values()]
+     final_features = [np.array(int_features)]
 #     prediction = model.predict(final_features)
 
 #     output = prediction[0]
 
-    return render_template('index.html', prediction_text='Gender: {}'.format('Pramod'))
+    return render_template('index.html', prediction_text='Gender: {}'.format(final_features[0]))
 
 
 if __name__ == "__main__":

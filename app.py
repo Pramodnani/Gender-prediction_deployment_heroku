7f10f72d9d1a725f1bb1b423d4b7ae5aa437355a
Pramodnani <48893435+Pramodnani@users.noreply.github.com>
2020-03-17 02:43:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def predict():
 
 #     output = prediction[0]
 
-    return render_template('index.html', prediction_text='Gender: {}'.format(final_features[0]))
+    return render_template('index.html', prediction_text='Gender: {}'.format(final_features))
 
 
 if __name__ == "__main__":

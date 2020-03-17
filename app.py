956dca5420f81ff32a29fe6c7c65e87392df6a2d
Pramodnani <48893435+Pramodnani@users.noreply.github.com>
2020-03-17 02:33:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,18 +9,18 @@ model = pickle.load(open('model.pkl', 'rb'))
 def home():
     return render_template('index.html')
 
-# @app.route('/predict',methods=['POST'])
-# # def predict():
-# #     '''
-# #     For rendering results on HTML GUI
-# #     '''
-# #     int_features = [int(x) for x in request.form.values()]
-# #     final_features = [np.array(int_features)]
-# #     prediction = model.predict(final_features)
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+#     int_features = [int(x) for x in request.form.values()]
+#     final_features = [np.array(int_features)]
+#     prediction = model.predict(final_features)
 
-# #     output = prediction[0]
+#     output = prediction[0]
 
-# #     return render_template('index.html', prediction_text='Gender: {}'.format(output))
+    return render_template('index.html', prediction_text='Gender: {}'.format('Pramod'))
 
 
 if __name__ == "__main__":

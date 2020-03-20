2731bdbecff5866b830d1f62036652bfb58342e5
Pramodnani <48893435+Pramodnani@users.noreply.github.com>
2020-03-20 18:00:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,12 +17,11 @@ def predict():
     '''
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-
+    prediction = model.predict([[175,170]])
     output = prediction[0]
-
     return render_template('index.html', prediction_text='Gender: {}'.format(output))
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
